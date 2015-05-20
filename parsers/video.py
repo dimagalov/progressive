665c75dc1da0be569329a5fd8b9d182dc9c233fd
Dima Galov <dimagalov@gmail.com>
2015-05-20 14:36:02 +0400

    Exceptions handling added to video parser

--- parsers/video.py
+++ parsers/video.py
@@ -7,48 +7,78 @@ from common.tools import cook_soup_from_url, get_current_timestamp
 
 
 def get_video_id(video_soup):
-    return video_soup["data-vid"]
+    try:
+        return video_soup["data-vid"]
+    except:
+        print("Problem occured while getting video id")
 
 
 def get_video_name(video_soup):
-    return video_soup.find("div", class_="video_row_info_name").a.string.strip()
+    try:
+        return video_soup.find("div", class_="video_row_info_name").a.string.strip()
+    except:
+        print("Problem occured while getting video name")
 
 
 def get_owner_id(video_soup):
-    return "https://vk.com" + video_soup.find("a", class_="mem_link")["href"]
+    try:
+        return "https://vk.com" + video_soup.find("a", class_="mem_link")["href"]
+    except:
+        print("Problem occured while getting owner id")
 
 
 def get_owner_name(video_soup):
-    return video_soup.find("a", class_="mem_link").string
+    try:
+        return video_soup.find("a", class_="mem_link").string
+    except:
+        print("Problem occured while getting owner name")
 
 
 def get_publication_date(video_soup):
-    return video_soup.find("span", class_="video_row_info_date rel_date_needs_update").string
+    try:
+        return video_soup.find("span", class_="video_row_info_date rel_date_needs_update").string
+    except:
+        print("Problem occured while getting publication date")
 
 
 def get_video_length(video_soup):
-    return video_soup.find("div", class_="video_row_duration video_row_count").string
+    try:
+        return video_soup.find("div", class_="video_row_duration video_row_count").string
+    except:
+        print("Problem occured while getting video length")
 
 
 def get_views_amount(video_soup):
-    amount_info = str(video_soup.find("span", class_="video_row_info_views"))
-    amount_info = amount_info.replace("<span class=\"num_delim\"> </span>", "")
-    amount_info = amount_info.replace("<span class=\"video_row_info_views\">", "")
-    amount_info = amount_info.replace("</span>", "")
-    amount_info = amount_info.strip()
-    return amount_info
+    try:
+        amount_info = str(video_soup.find("span", class_="video_row_info_views"))
+        amount_info = amount_info.replace("<span class=\"num_delim\"> </span>", "")
+        amount_info = amount_info.replace("<span class=\"video_row_info_views\">", "")
+        amount_info = amount_info.replace("</span>", "")
+        amount_info = amount_info.strip()
+        return amount_info
+    except:
+        print("Problem occured while getting views amount")
 
 
 def get_likes_amount(video_soup):
-    return "None"
+    try:
+        return "None"
+    except:
+        print("Problem occured while getting likes amount")
 
 
 def get_video_link(video_soup):
-    return "https://vk.com/video" + get_video_id(video_soup)
+    try:
+        return "https://vk.com/video" + get_video_id(video_soup)
+    except:
+        print("Problem occured while getting video link")
 
 
 def get_video_description(video_soup):
-    return "None"
+    try:
+        return "None"
+    except:
+        print("Problem occured while getting video description")
 
 
 def parse_video(video_soup):
@@ -69,7 +99,10 @@ def parse_video(video_soup):
 
 
 def get_video_list(soup):
-    return soup.find_all('div', class_="video_row clear_fix")
+    try:
+        return soup.find_all('div', class_="video_row clear_fix")
+    except:
+        print("Problem occured while getting video list")
 
 
 def parse_video_page(url):
