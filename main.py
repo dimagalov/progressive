9c40721410d64b137663225105da2406eb3f4d77
Dima Galov <dimagalov@gmail.com>
2015-05-24 21:13:10 +0400

    Basic ser wall parser completed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ __author__ = 'dimagalov'
 # from parsers.group import parse_group_page
 # from parsers.photo import parse_photo_page
 # from parsers.video import parse_video_page
+# from parsers.post import parse_post
 # from parsers.user_wall import parse_user_wall
 # from parsers.group_wall import parse_group_wall
 
@@ -14,5 +15,6 @@ __author__ = 'dimagalov'
 # parse_group_page('https://vk.com/public43618728')
 # parse_photo_page('https://vk.com/photos53083705')
 # parse_video_page('https://vk.com/videos53083705')
-# parse_user_wall('https://vk.com/wall53083705?own=1')
+# parse_post('https://vk.com/wall53083705_52454')
+# parse_user_wall('https://vk.com/dm?offset=5&amp;own=20#posts')
 # parse_group_wall('https://vk.com/wall-43618728?own=1')
--- parsers/user_wall.py
+++ parsers/user_wall.py
@@ -2,5 +2,30 @@
 
 __author__ = 'dimagalov'
 
-from common.models import User_Wall, Post
+from common.models import User_Wall
+from parsers.post import parse_post
 from common.tools import cook_soup_from_url, get_current_timestamp
+
+
+def get_user(soup):
+    pass
+
+
+def get_user_posts(soup):
+    pass
+
+
+def parse_user_wall(url):
+    soup = cook_soup_from_url(url)
+
+    print(soup.prettify())
+
+    return
+
+    user = get_user(soup)
+    posts = get_user_posts(soup)
+
+    user_wall = User_Wall(user, posts)
+    print(user_wall)
+
+    return User_Wall()
