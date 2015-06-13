10bf856a03e3203e87817ed4ddd88e17f1be6f5f
Dima Galov <dimagalov@gmail.com>
2015-06-13 21:01:39 +0400

    Main cleaned

--- vk_api_parser/main.py
+++ vk_api_parser/main.py
@@ -3,14 +3,14 @@
 __author__ = 'dimagalov'
 
 
-from parsers.user import parse_user_pages
+# from parsers.user import parse_user_pages
 # from parsers.group import parse_group_pages
 
-dimagalov = parse_user_pages("dimagalov")[0]
-print(dimagalov)
+# dimagalov = parse_user_pages("dimagalov")[0]
+# print(dimagalov)
 
-for post in dimagalov.wall.posts:
-    print(post)
+# for post in dimagalov.wall.posts:
+    # print(post)
 
 # doner = parse_group_pages("topdonersuka")[0]
 # print(doner)
