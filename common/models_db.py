44eed6ab6cb2e62b8819a0a5c4c0aa4e2d5a75b9
Roman Lozko <lozko@179.ru>
2015-08-04 17:08:02 +0300

    Update models_db.py

--- common/models_db.py
+++ common/models_db.py
@@ -166,10 +166,7 @@ class Attachments:
         self.amount = 0
         self.list_of_attachments = []
         
-        return  # TODO: repair attachments
-                # ASSIGNEE: anybody
-                # TODO: rewrite using execute callback model
-                # ASSIGNEE: vient
+        return              #  <<<----------------------------------------------------
 
         for attachment in list_of_attachments:
             timestamp = get_current_timestamp()
